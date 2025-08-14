8730d7891313784f3bf1783ca3360c7562e27986
MohamedBouafif <mohamedbouafif2020@gmail.com>
2025-08-14 19:31:21 +0100

    dsu template

--- coaching/learning lessons/graph&tree/DSU/dsu.cpp
+++ coaching/learning lessons/graph&tree/DSU/dsu.cpp
@@ -7,9 +7,9 @@ class Dsu{
         
         Dsu(int k){
             n = k;
-            parent = vector<int>(n+1);
-            rank = vector<int>(n+1)
-            for(int i = 1 ; i<= n;i++){
+            parent.resize(n);
+            rank.resize(n);
+            for(int i = 0 ; i< n;i++){
                 parent[i] = i;
                 rank[i] = 1;
             }
@@ -19,18 +19,21 @@ class Dsu{
             if(parent[i] != i){
                 return parent[i] = find(parent[i]);
             }
-            return parent[i] = i;
+            return parent[i];
         }
 
         void unionn(int a ,int b){
             int p1 = find(a);
             int p2 = find(b);
-            
-            if(rank[p1] < rank[p2]){
-                swap(p1 , p2);
+            if(p1 != p2)
+            {
+                if(rank[p1] < rank[p2])
+                {
+                    swap(p1 , p2);
+                }
+                parent[p2] = p1;
+                rank[p1] += rank[p2];
+                rank[p2] = 0;
             }
-            parent[p2] = p1;
-            rank[p1] += rank[p2];
-            rank[p2] = 0;
         }
 };
