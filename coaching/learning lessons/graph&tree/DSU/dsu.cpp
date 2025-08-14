73148257b2b9be6ac8e3e9d6d9d7003ca86d6825
MohamedBouafif <mohamedbouafif2020@gmail.com>
2025-08-14 19:31:04 +0100

    dsu template

--- coaching/learning lessons/graph&tree/DSU/dsu.cpp
+++ coaching/learning lessons/graph&tree/DSU/dsu.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-class DSU{
+class Dsu{
     public:
-        map<int,int> parent;
-        map<int,int> rank;
+        vector<int> parent, rank;
         int n;
         
-        DSU(vector<int>& nums){
-            for(auto e:nums){
-                parent[e] = e;
-                rank[e] = 1;
+        Dsu(int k){
+            n = k;
+            parent = vector<int>(n+1);
+            rank = vector<int>(n+1)
+            for(int i = 1 ; i<= n;i++){
+                parent[i] = i;
+                rank[i] = 1;
             }
         }
 
@@ -25,44 +27,10 @@ class DSU{
             int p2 = find(b);
             
             if(rank[p1] < rank[p2]){
-                parent[p1] = p2;
-                rank[p2]+= rank[p1];
-                rank[p1] = 0;
-            }else{
-                parent[p2] = p1;
-                rank[p1]+= rank[p2];
-                rank[p2] = 0;
+                swap(p1 , p2);
             }
+            parent[p2] = p1;
+            rank[p1] += rank[p2];
+            rank[p2] = 0;
         }
-};
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        int  n = nums.size();
-        if(n==0)return 0;
-        int ans = 1; 
-        DSU dsu = DSU(nums);
-        multiset<int> s;
-        for(int i = 0 ; i < n ;i++){
-            if(!s.empty()){
-
-                if(s.find(nums[i])!=s.end())continue;
-
-                if(s.find(nums[i] - 1) !=s.end()){
-                    dsu.unionn(nums[i] , nums[i] - 1);
-                    int p1 = dsu.parent[nums[i]], p2 = dsu.parent[nums[i] - 1];
-                    ans = max({dsu.rank[p1], dsu.rank[p2], ans});
-                }
-                
-                if(s.find(nums[i] + 1) !=s.end()){
-                    dsu.unionn(nums[i] , nums[i] + 1);
-                    int p1 = dsu.parent[nums[i]], p2 = dsu.parent[nums[i] + 1];
-                    ans = max({dsu.rank[p1], dsu.rank[p2], ans});
-                }
-                
-            }
-            s.insert(nums[i]);
-        }
-        return ans;
-    }
 };
